f41fc02ecbb8fb3927cb06166f42034d1bb387a0
suryateja <suryatejayalla@gmail.com>
2021-12-28 15:42:17 +0530

    new files added

---
+++ main.tf
@@ -0,0 +1,39 @@
+data "aws_vpc" "vpc1" {
+  cidr_block       = "10.0.0.0/16"
+
+}
+
+data "aws_subnet" "subnet1" {
+  vpc_id     = "${data.aws_vpc.vpc1.id}"
+  cidr_block = "10.0.1.0/24"
+
+}
+
+
+resource "aws_network_interface" "ni" {
+  subnet_id   = "${data.aws_subnet.subnet1.id}"
+  tags = {
+    Name = "primary_network_interface"
+  }
+}
+
+resource "aws_instance" "ec2" {
+  ami           = "ami-0567e0d2b4b2169ae"
+  instance_type = "t2.micro"
+  key_name      = "ohio"
+
+  network_interface {
+    network_interface_id = aws_network_interface.ni.id
+    device_index         = 0
+  }
+
+  credit_specification {
+    cpu_credits = "unlimited"
+
+  }
+
+  tags = {
+    Name = "coupa"
+  }
+
+}
